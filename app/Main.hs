d44507aec42e15399f1cf29078d7e5e7cba5b96d
Austin Erlandson <austin.erlandson@metova.com>
2016-08-07 19:58:37 -0500

    Working on the new A&Em static site built on Hakyll...

---
+++ app/Main.hs
@@ -0,0 +1,76 @@
+--------------------------------------------------------------------------------
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyllWith config $ do
+  match "assets/images/*" $ do
+    route   $ gsubRoute "assets/" (const "")
+    compile $ copyFileCompiler
+
+  match "styles.css" $ do
+    route   idRoute
+    compile compressCssCompiler
+
+  match (fromList ["about.rst", "contact.markdown"]) $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
+
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts) `mappend`
+            constField "title" "Archives"            `mappend`
+            defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
+
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts) `mappend`
+            constField "title" "Home"                `mappend`
+            defaultContext
+
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+  dateField "date" "%B %e, %Y" `mappend`
+  defaultContext
+
+config :: Configuration
+config = defaultConfiguration
+  { providerDirectory = "app"
+  }
